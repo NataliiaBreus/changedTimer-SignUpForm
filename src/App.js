10d106f05edb85f6c1627f48346496fa069735d9
NataliiaBreus <breusnataly@gmail.com>
2021-05-27 07:15:47 +0300

    signUpFrom changedTimer

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import React, { useState } from "react";
+import SelectedUserList from "./components/SelectedUserList";
+import UserList from "./components/UserList";
+import AlohaDashboard from "./components/AlohaDashboard";
+
+const usersDB = [
+  {
+    id: 1,
+    firstname: "John",
+    lastname: "Smith",
+  },
+  {
+    id: 2,
+    firstname: "Bob",
+    lastname: "Brown",
+  },
+  {
+    id: 3,
+    firstname: "Sara",
+    lastname: "Smith",
+  },
+  {
+    id: 4,
+    firstname: "Mary",
+    lastname: "Black",
+  },
+];
+
+/*
+  Перепишите Aloha, App как функциональные компоненты
+*/
+
+function App(props) {
+  const defaultUsers = usersDB.map((user) => ({
+    ...user,
+    isSelected: false,
+  }));
+  const [users, setUsers] = useState(defaultUsers);
+  const [isHidden, setIsHidden] = useState(false);
+  const hideHandler = () => setIsHidden(!isHidden);
+
+  return (
+    <>
+      <header>
+        <SelectedUserList users={users} />
+      </header>
+      <main>
+        <AlohaDashboard />
+        <button onClick={hideHandler}>Hide</button>
+        <UserList users={users} setUsers={setUsers} />
+      </main>
+    </>
+  );
+}
+export default App;
